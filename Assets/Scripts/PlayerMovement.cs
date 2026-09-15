90475e9d4331bdf76c950d35e6a688bffa09e280
Tushar280 <chaudarytushar280@gmail.com>
2026-09-15 19:17:36 +0530

    Updates

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+
+    private Rigidbody rb;
+    private Vector3 moveInput;
+    private PlayerInput playerInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        playerInput = new PlayerInput();
+    }
+
+    void Update()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+
+    void OnMovement(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    void MovePlayer()
+    {
+        Vector3 direction = transform.right * moveInput.x + transform.forward * moveInput.y;
+        direction.Normalize();
+        rb.linearVelocity = new Vector3(direction.x * moveSpeed, rb.linearVelocity.y, direction.z * moveSpeed);
+    }
+}
