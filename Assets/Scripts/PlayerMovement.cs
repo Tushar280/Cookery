d7b40329e2a04c1363b834c751348344ebd6c34d
Tushar280 <chaudarytushar280@gmail.com>
2026-09-15 19:20:15 +0530

    updates

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,7 +13,7 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        playerInput = new PlayerInput();
+        playerInput = GetComponent<PlayerInput>();
     }
 
     void Update()
