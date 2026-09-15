8ceb99d2e182acc9635c5f0cf614d0bb12cf0946
Tushar280 <chaudarytushar280@gmail.com>
2026-09-15 20:20:54 +0530

    updates

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,14 +5,13 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float rotateSpeed = 10f;
+    [SerializeField] private float playerSize = 0.7f;
 
-    private Rigidbody rb;
     private Vector2 moveInput;
     private PlayerInput playerInput;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
         playerInput = GetComponent<PlayerInput>();
     }
 
@@ -20,25 +19,21 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector3 moveDir = new Vector3(moveInput.x, 0f, moveInput.y);
 
+        bool canMove = !Physics.Raycast(transform.position, moveDir, playerSize);
+
+        if (canMove)
+        {
+            transform.position += moveDir * moveSpeed * Time.deltaTime;
+        }
+
         if (moveDir != Vector3.zero)
         {
             transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
         }
     }
 
-    private void FixedUpdate()
-    {
-        MovePlayer();
-    }
-
     void OnMovement(InputValue value)
     {
         moveInput = value.Get<Vector2>();
-    }
-
-    void MovePlayer()
-    {
-        Vector3 moveDir = new Vector3(moveInput.x, 0f, moveInput.y);
-        rb.linearVelocity = new Vector3(moveDir.x * moveSpeed, rb.linearVelocity.y, moveDir.z * moveSpeed);
     }
 }
